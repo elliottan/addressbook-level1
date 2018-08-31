3023a0633e17bb54e1334acd0803945fa1b65a61
Elliot Tan <elliottan@gmail.com>
2018-08-31 10:33:35 +0800

    Updated the version.

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -44,7 +44,7 @@ public class AddressBook {
     /**
      * Version info of the program.
      */
-    private static final String VERSION = "AddessBook Level 1 - Version 1.0";
+    private static final String VERSION = "AddessBook Level 1 - Version 1.0.1s";
 
     /**
      * A decorative prefix added to the beginning of lines printed by AddressBook
